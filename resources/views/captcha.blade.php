eada8ae41709407eed0a7b6301137b2319b55114
yaozm <ityaozm@gmail.com>
2021-07-23 10:28:51 +0800

    Optimize captcha blade

--- resources/views/captcha.blade.php
+++ resources/views/captcha.blade.php
@@ -1,14 +1,14 @@
 (function () {
     var captchaTpl;
-    captchaTpl = '<fieldset class="form-label-group form-group position-relative has-icon-left lake-login-captcha">'
-    captchaTpl += '<input id="captcha" type="text" style="width: 61%;" class="form-control" name="captcha" placeholder="验证码" required>'
-    captchaTpl += '<span class="captcha-img" style="width: 37%;height: 34px;position: absolute;top: 0;right: 0;border-radius: .25rem;border: 1px solid #dbe3e6;">'
-    captchaTpl += '<img id="verify" src="{{ $captchaUrl }}" data-src="{{ $captchaUrl }}" alt="验证码" title="刷新验证码" class="captcha" style="cursor: pointer;width: 100%;border-radius: .25rem;">'
+    captchaTpl = '<fieldset class="form-label-group form-group position-relative has-icon-left dcat-login-captcha">'
+    captchaTpl += '<input id="captcha" type="text" style="width: 61%;" class="form-control" name="{{ \Guanguans\DcatLoginCaptcha\DcatLoginCaptchaServiceProvider::trans('login_captcha.captcha') }}" placeholder="{{ \Guanguans\DcatLoginCaptcha\DcatLoginCaptchaServiceProvider::trans('login_captcha.captcha') }}" required>'
+    captchaTpl += '<span class="captcha-img" style="width: 37%;height: 33.5px;position: absolute;top: 0;right: 0;border-radius: .25rem;border: 1px solid #dbe3e6;">'
+    captchaTpl += '<img id="verify" src="{{ $captchaUrl }}" data-src="{{ $captchaUrl }}" alt="{{ \Guanguans\DcatLoginCaptcha\DcatLoginCaptchaServiceProvider::trans('login_captcha.captcha') }}" title="{{ \Guanguans\DcatLoginCaptcha\DcatLoginCaptchaServiceProvider::trans('login_captcha.refresh_captcha') }}" class="captcha" style="cursor: pointer;width: 100%;height: 100%;border-radius: .25rem;">'
     captchaTpl += '</span>'
     captchaTpl += '<div class="form-control-position">'
     captchaTpl += '<i class="feather icon-image"></i>'
     captchaTpl += '</div>'
-    captchaTpl += '<label for="captcha">验证码</label>'
+    captchaTpl += '<label for="captcha">{{ \Guanguans\DcatLoginCaptcha\DcatLoginCaptchaServiceProvider::trans('login_captcha.captcha') }}</label>'
     captchaTpl += '<div class="help-block with-errors"></div>'
     captchaTpl += '</fieldset>';
 
@@ -19,7 +19,7 @@
         $(this).attr("src", verifyImg.replace(/\?.*$/, "") + "?" + Math.random());
     });
 
-    $(".lake-login-captcha .with-errors").bind("DOMNodeInserted", function () {
+    $(".dcat-login-captcha .with-errors").bind("DOMNodeInserted", function () {
         if ($("#captcha").val() !== "" && $(this).html().length > 0) {
             $("#verify").trigger("click");
         }
