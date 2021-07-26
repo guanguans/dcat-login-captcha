bef95ca6179273853f2118a0f3e281839e6f049d
yaozm <ityaozm@gmail.com>
2021-07-26 09:59:16 +0800

    Remove src/Http/Controllers/CaptchaController.php`

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -8,14 +8,15 @@
  * This source file is subject to the MIT license that is bundled.
  */
 
-use Guanguans\DcatLoginCaptcha\Http\Controllers\CaptchaController;
 use Guanguans\DcatLoginCaptcha\Http\Middleware\CleanObContents;
 use Guanguans\DcatLoginCaptcha\Http\Middleware\SetResponseContentType;
 use Illuminate\Support\Facades\Route;
 
-Route::get('captcha/generate', sprintf('%s@generate', CaptchaController::class))
-    ->name('captcha.generate')
-    ->middleware([
-        SetResponseContentType::class,
-        CleanObContents::class,
-    ]);
+Route::get('captcha/generate', function () {
+    return response(login_captcha_get());
+})
+->name('captcha.generate')
+->middleware([
+    SetResponseContentType::class,
+    CleanObContents::class,
+]);
