f2c5fbc90256438d38699d7cda3bf00019dbb26c
yaozm <ityaozm@gmail.com>
2021-07-31 18:22:59 +0800

    Optimize `buildCaptchaJsScript`

--- src/BootingHandler.php
+++ src/BootingHandler.php
@@ -29,7 +29,9 @@ class BootingHandler
         }
 
         if (Helper::matchRequestPath('POST:admin/auth/login')) {
-            $validator = Validator::make(Request::post(), ['captcha' => 'required|dcat_login_captcha']);
+            $validator = Validator::make(Request::post(), [
+                'captcha' => 'required|dcat_login_captcha',
+            ]);
 
             $validator->fails() and $this->throwHttpResponseException($validator);
         }
@@ -40,9 +42,40 @@ class BootingHandler
      */
     protected function buildCaptchaScript()
     {
-        return (string) view('guanguans.dcat-login-captcha::captcha', [
-            'captchaUrl' => \login_captcha_url(),
-        ]);
+        [$captchaUrl, $langCaptcha, $langRefreshCaptcha] = [
+            login_captcha_url(),
+            LoginCaptchaServiceProvider::trans('login_captcha.captcha'),
+            LoginCaptchaServiceProvider::trans('login_captcha.refresh_captcha'),
+        ];
+
+        return <<<js
+(function () {
+    captchaTpl = '<fieldset class="form-label-group form-group position-relative has-icon-left dcat-login-captcha">';
+    captchaTpl += '<input id="captcha" type="text" style="width: 61%;" class="form-control" name="captcha" placeholder="$langCaptcha" required />';
+    captchaTpl += '<span class="captcha-img" style="width: 37%;height: 33.5px;position: absolute;top: 0;right: 0;border-radius: .25rem;border: 1px solid #dbe3e6;">';
+    captchaTpl += '<img id="verify" src="$captchaUrl" data-src="$captchaUrl" alt="$langCaptcha" title="$langRefreshCaptcha" class="captcha" style="cursor: pointer;width: 100%;height: 100%;border-radius: .25rem;">';
+    captchaTpl += '</span>';
+    captchaTpl += '<div class="form-control-position">';
+    captchaTpl += '<i class="feather icon-image"></i>';
+    captchaTpl += '</div>';
+    captchaTpl += '<label for="captcha">$langCaptcha</label>';
+    captchaTpl += '<div class="help-block with-errors"></div>';
+    captchaTpl += '</fieldset>';
+
+    $(captchaTpl).insertAfter($("#login-form fieldset.form-label-group").get(1));
+
+    $("#verify").click(function () {
+        verifyImg = $(this).data("src");
+        $(this).attr("src", verifyImg.replace(/\?.*$/, "") + "?" + Math.random());
+    });
+
+    $(".dcat-login-captcha .with-errors").bind("DOMNodeInserted", function () {
+        if ($("#captcha").val() !== "" && $(this).html().length > 0) {
+            $("#verify").trigger("click");
+        }
+    });
+})();
+js;
     }
 
     /**
