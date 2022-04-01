30e89908b1751f7af90f232f0a955eff7f9b4d16
yaozm <ityaozm@gmail.com>
2022-04-01 11:25:14 +0800

    Replace `Closure routing` -> `CaptchaController`

---
+++ src/Http/Controllers/CaptchaController.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of the guanguans/dcat-login-captcha.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\DcatLoginCaptcha\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+
+class CaptchaController extends Controller
+{
+    public function generate()
+    {
+        return response(login_captcha_get());
+    }
+}
