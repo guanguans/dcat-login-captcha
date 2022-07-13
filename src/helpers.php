05d36a1659faefc134aa7bc20a32ed4a82c2299a
yaozm <ityaozm@gmail.com>
2022-07-13 15:53:41 +0800

    Rename login_captcha_get -> login_captcha_content

--- src/helpers.php
+++ src/helpers.php
@@ -40,11 +40,11 @@ if (! function_exists('login_captcha_url')) {
     }
 }
 
-if (! function_exists('login_captcha_get')) {
+if (! function_exists('login_captcha_content')) {
     /**
      * 获取登录验证码图像内容.
      */
-    function login_captcha_get(int $quality = 90): string
+    function login_captcha_content(int $quality = 90): string
     {
         Session::put(LoginCaptchaServiceProvider::setting('captcha_phrase_session_key'), CaptchaBuilder::getPhrase());
 
