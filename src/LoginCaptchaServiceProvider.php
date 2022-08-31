eb02afa3b9b9aec68e9c59f0ee59098e16d1f60b
yaozm <ityaozm@gmail.com>
2022-08-31 16:09:33 +0800

    加载迁移文件

--- src/LoginCaptchaServiceProvider.php
+++ src/LoginCaptchaServiceProvider.php
@@ -38,6 +38,7 @@ class LoginCaptchaServiceProvider extends ServiceProvider
     {
         parent::init();
         $this->setupConfig();
+        // $this->loadMigrationsFrom(__DIR__.'/../updates/update_admin_settings_for_dcat_login_captcha.php');
         $this->extendValidator();
         Admin::booting($this->app->make(BootingHandler::class));
     }
