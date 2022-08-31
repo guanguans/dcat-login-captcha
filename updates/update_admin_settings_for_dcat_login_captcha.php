e031d601d5da2e7915bc0ed66682c1e7120d677c
yaozm <ityaozm@gmail.com>
2022-08-31 15:53:03 +0800

    Add migration files

---
+++ updates/update_admin_settings_for_dcat_login_captcha.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file is part of the guanguans/dcat-login-captcha.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+use Dcat\Admin\Models\Setting;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateAdminSettingsForDcatLoginCaptcha extends Migration
+{
+    public function getConnection()
+    {
+        return $this->config('database.connection', config('database.default'));
+    }
+
+    public function config($key, $default = null)
+    {
+        return config('admin.'.$key, $default);
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $setting = Setting::query()
+            ->where('slug', 'guanguans:dcat-login-captcha')
+            ->firstOrFail()
+            ->mergeCasts(['value' => 'array']);
+
+        $diff = array_diff_key(config('login_captcha'), $value = $setting->value);
+        $setting->value = array_merge($value, $diff);
+        $setting->save();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
