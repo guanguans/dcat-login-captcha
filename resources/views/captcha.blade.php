52034c93f7b9f8ee1c0d95ddddcd76bbcf9ecee4
yaozm <ityaozm@gmail.com>
2023-10-17 17:27:00 +0800

    refactor(captcha): refactor captcha blade template
    - Refactor the captcha blade template to improve readability and maintainability.
    - Update the HTML structure and class names for better consistency.

--- resources/views/captcha.blade.php
+++ resources/views/captcha.blade.php
@@ -1,25 +1,36 @@
 (function () {
-    const captchaDOM = `
-        <fieldset class="form-label-group form-group position-relative has-icon-left dcat-login-captcha">
-            <input id="captcha" class="form-control" type="text" style="width: 61%;" name="captcha" placeholder="{{ \Guanguans\DcatLoginCaptcha\LoginCaptchaServiceProvider::trans('login-captcha.captcha') }}" required>
-            <span class="captcha-img" style="width: 37%;height: 33.5px;position: absolute;top: 0;right: 0;border-radius: .25rem;border: 1px solid #dbe3e6;">
-                <img id="verify" class="captcha" src="{{ login_captcha_url() }}" data-src="{{ login_captcha_url() }}" alt="{{ \Guanguans\DcatLoginCaptcha\LoginCaptchaServiceProvider::trans('login-captcha.captcha') }}" title="{{ \Guanguans\DcatLoginCaptcha\LoginCaptchaServiceProvider::trans('login-captcha.refresh_captcha') }}" style="cursor: pointer;width: 100%;height: 100%;border-radius: .25rem;">
-            </span>
-            <div class="form-control-position"><i class="feather icon-image"></i></div>
-            <label for="captcha">{{ \Guanguans\DcatLoginCaptcha\LoginCaptchaServiceProvider::trans('login-captcha.captcha') }}</label>
-            <div class="help-block with-errors"></div>
-        </fieldset>
-    `;
+const captchaDOM = `
+<fieldset class="form-label-group form-group position-relative has-icon-left" id="captcha">
+    <input
+        class="form-control" id="captcha-input" name="captcha"
+        placeholder="{{ Guanguans\DcatLoginCaptcha\LoginCaptchaServiceProvider::trans('login-captcha.captcha') }}"
+        required style="width: 61%;"
+        type="text"
+    >
+    <span style="width: 37%;height: 33.5px;position: absolute;top: 0;right: 0;border-radius: .25rem;border: 1px solid #dbe3e6;">
+        <img
+            alt="{{ Guanguans\DcatLoginCaptcha\LoginCaptchaServiceProvider::trans('login-captcha.captcha') }}"
+            id="captcha-img"
+            src="{{ login_captcha_url() }}"
+            style="cursor: pointer;width: 100%;height: 100%;border-radius: .25rem;"
+            title="{{ Guanguans\DcatLoginCaptcha\LoginCaptchaServiceProvider::trans('login-captcha.refresh_captcha') }}"
+        >
+    </span>
+    <div class="form-control-position"><i class="feather icon-image"></i></div>
+    <label for="captcha-input">{{ Guanguans\DcatLoginCaptcha\LoginCaptchaServiceProvider::trans('login-captcha.captcha') }}</label>
+    <div class="help-block with-errors"></div>
+</fieldset>
+`;
 
-    $(captchaDOM).insertAfter($('#login-form fieldset.form-label-group').get(1));
+$(captchaDOM).insertAfter($('#login-form fieldset.form-label-group').get(1));
 
-    $('#verify').click(function () {
-        $(this).attr('src', $(this).data('src').replace(/\?.*$/, '') + '?' + Math.random());
-    });
+$('#captcha-img').click(function () {
+    $(this).attr('src', $(this).attr('src').replace(/\?random=.*$/, '?random=' + Math.random()));
+});
 
-    $('.dcat-login-captcha .with-errors').bind('DOMNodeInserted', function () {
-        if ($('#captcha').val() !== '' && $(this).html().length > 0) {
-            $("#verify").trigger("click");
-        }
-    });
+$('#captcha .with-errors').bind('DOMNodeInserted', function () {
+    if ($('#captcha-input').val() !== '' && $(this).html().length > 0) {
+        $("#captcha-img").trigger("click");
+    }
+});
 })();
